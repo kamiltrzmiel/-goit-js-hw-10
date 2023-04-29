c48abf01d4c7b447aceee0c0eda3cf368db0f607
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-04-29 13:09:44 +0200

    all done final

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,18 @@
+import Notiflix from 'notiflix';
+
 const API_URL = 'https://restcountries.com/v3.1/name/';
 
 export const fetchCountries = name => {
-  return fetch(`${API_URL}/${name}?fields=name,capital,population,flags,languages`).then(response =>
-    response.json()
-  );
+  return fetch(`${API_URL}/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      setTimeout(() => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }, 1200);
+    });
 };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const DEBOUNCE_DELAY = 300;
 const searchEl = document.getElementById('search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
+const backgroundEl = document.querySelector('body');
 
 const clrScreen = () => {
   countryListEl.innerHTML = '';
@@ -14,39 +15,37 @@ const clrScreen = () => {
 
 searchEl.addEventListener(
   'input',
-  _.debounce(async e => {
-    try {
-      const countryName = e.target.value.trim();
-      const countries = await fetchCountries(countryName);
+  _.debounce(async event => {
+    const countryName = event.target.value.trim();
+    const countries = await fetchCountries(countryName);
 
-      if (countryName === '') {
-        return;
-      }
+    if (countryName === '') {
+      return;
+    }
 
-      if (countries.length > 10) {
-        clrScreen();
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      } else if (countries.length >= 2 && countries.length <= 10) {
-        clrScreen();
-        countryListEl.innerHTML = countries
-          .map(
-            country =>
-              `<li><img height="32" src="${country.flags.svg}" />${country.name.common}</li>`
-          )
-          .join('');
-      }
+    if (countries.length > 10) {
+      clrScreen();
+      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (countries.length >= 2 && countries.length <= 10) {
+      clrScreen();
+      countryListEl.innerHTML = countries
+        .map(
+          country => `<li><img height="32" src="${country.flags.svg}" />${country.name.common}</li>`
+        )
+        .join('');
+    }
 
-      if (countries.length === 1) {
-        clrScreen();
-        countryInfoEl.innerHTML = `
+    if (countries.length === 1) {
+      clrScreen();
+      const cptlName = countries[0].capital;
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + cptlName + "')";
+      countryInfoEl.innerHTML = `
         <p><img height="64" src="${countries[0].flags.svg}" /></p>
         <p>${countries[0].name.common}</p>
         <p>Capital: ${countries[0].capital}</p>
         <p>Population: ${countries[0].population}</p>
         <p>Languages: ${Object.values(countries[0].languages).join(', ')}</p>`;
-      }
-    } catch (error) {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
     }
   }, DEBOUNCE_DELAY)
 );
